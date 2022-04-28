0de8a450b7cc347ebe90945036dadcad613e6c54
1711029 <104539588+1711029@users.noreply.github.com>
2022-04-28 09:13:36 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 basic.forever(function () {
-	
+    basic.showString("jason")
+    basic.clearScreen()
+    basic.pause(5000)
+    basic.showString("5a23")
+    basic.clearScreen()
+    basic.pause(5000)
+    basic.showIcon(IconNames.Sword)
 })
